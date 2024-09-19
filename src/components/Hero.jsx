9f35dbecd092138f6c849620e5a338b36a025f57
manueljo <mikebillyy@gmail.com>
2024-09-19 14:14:24 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto p-4">
+      <div className="flex flex-col gap-4">
+        <p>It's time to get</p>
+        <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          SWOLE<span className="text-blue-400">NORMOUS</span>
+        </h1>
+      </div>
+      <p className="text-sm md:text-base font-light">
+        I hereby acknowledgement that I may become
+        <span className="text-blue-400 font-medium">
+          unbelievably swolenormous
+        </span>
+        and accept all risks of becoming the local
+        <span className="text-blue-400 font-medium">mass montrosity</span>,
+        afflicted with severe body dismorphia, unable to fit through doors.
+      </p>
+      <button className="px-8 py-4 rounded-md border border-blue-400 border-solid border-[2px] bg-slate-950 blueshadow duration-200"> 
+        <p>Accept & Begin</p>
+      </button>
+    </div>
+  );
+}
+
+export default Hero;
